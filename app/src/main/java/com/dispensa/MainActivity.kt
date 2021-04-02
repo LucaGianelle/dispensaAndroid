360fb9477bf5a0ab9e6d97e7db7e0c39ca60445e
Davide <dbasilico1@studenti.uninsubria.it>
2021-04-02 11:32:46 +0200

    Collegamento bottoni

--- app/src/main/java/com/dispensa/MainActivity.kt
+++ app/src/main/java/com/dispensa/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.dispensa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        buttonAccesso.setOnClickListener {
+            val intent = Intent (this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
