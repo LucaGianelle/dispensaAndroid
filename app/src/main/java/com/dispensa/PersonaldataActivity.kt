92c82a49ee27aa923f54b6755283848e76c4ce1d
Davide <dbasilico1@studenti.uninsubria.it>
2021-04-02 14:51:09 +0200

    Collegamento bottoni, PersonaldataActivity

---
+++ app/src/main/java/com/dispensa/PersonaldataActivity.kt
@@ -0,0 +1,11 @@
+package com.dispensa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PersonaldataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
--- app/src/main/java/com/dispensa/RegisterActivity.kt
+++ app/src/main/java/com/dispensa/RegisterActivity.kt
@@ -1,11 +1,20 @@
 package com.dispensa
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+
+        buttonConferma.setOnClickListener {
+            val acc = Intent (this, MainActivity::class.java)
+            startActivity(acc)
+        }
+
     }
 }
