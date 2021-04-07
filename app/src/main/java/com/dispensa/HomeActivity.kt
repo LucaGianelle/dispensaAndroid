6cc8a203faa857fa21bbb61a17c4002434d4b26f
Federico <corna.federico.wkcf@gmail.com>
2021-04-07 10:44:09 +0200

    prima interazione con maps

--- app/src/main/java/com/dispensa/HomeActivity.kt
+++ app/src/main/java/com/dispensa/HomeActivity.kt
@@ -1,11 +1,38 @@
 package com.dispensa
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.home.*
+import java.util.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+
+        buttonMappa.setOnClickListener {
+
+            /*val latitude: Double = location.getLatitude()
+            val longitude: Double = location.getLongitude()
+
+            val uri: String = java.lang.String.format(Locale.ENGLISH, "geo:%f,%f", latitude, longitude)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+            context.startActivity(intent)*/
+
+            /*
+            *
+            *
+            *
+            */
+
+            val intent = Intent(Intent.ACTION_VIEW,
+                    /*Uri.parse("http://maps.google.com/maps?saddr=20.344,34.34&daddr=20.5666,45.345"))*/
+                    Uri.parse("google.navigation:q=an+address+city"))
+            startActivity(intent)
+        }
     }
+
+
 }
