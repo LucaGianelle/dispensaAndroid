071b604c1478781a99aea2b67f94a40c9b571d63
lgianelle <lgianelle@studenti.uninsubria.it>
2021-04-08 18:38:57 +0200

    number picker prototipo

--- app/src/main/java/com/dispensa/RegisterActivity.kt
+++ app/src/main/java/com/dispensa/RegisterActivity.kt
@@ -2,14 +2,21 @@ package com.dispensa
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.NumberPicker
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login.*
 import kotlinx.android.synthetic.main.register.*
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+
+
+        numberPicker.setMinValue(40);
+        numberPicker.setMaxValue(120);
+        numberPicker.wrapSelectorWheel = true
 
         buttonConferma.setOnClickListener {
             val acc = Intent (this, MainActivity::class.java)
@@ -17,4 +24,5 @@ class RegisterActivity : AppCompatActivity() {
         }
 
     }
+
 }
