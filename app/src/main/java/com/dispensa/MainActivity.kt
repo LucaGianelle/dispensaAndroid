a881fa9f05835941cf9b9ee78bfc7b968524bd5a
Davide <dbasilico1@studenti.uninsubria.it>
2021-04-22 16:09:31 +0200

    Autenticazione con Firebase

--- app/src/main/java/com/dispensa/MainActivity.kt
+++ app/src/main/java/com/dispensa/MainActivity.kt
@@ -3,12 +3,18 @@ package com.dispensa
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        auth = FirebaseAuth.getInstance()
 
         buttonAccesso.setOnClickListener {
             val acc = Intent (this, HomeActivity::class.java)
@@ -25,4 +31,17 @@ class MainActivity : AppCompatActivity() {
         }*/
 
     }
+
+    public override fun onStart() {
+        super.onStart()
+        setContentView(R.layout.login)
+            //controlla se l'utente è registrato, (valore non nullo) e aggiorna l'interfaccia di conseguenza
+        val currentUser = auth.currentUser
+        updateUI (currentUser)
+    }
+
+    fun updateUI(currentUser: FirebaseUser?) {
+
+    }
+
 }
--- app/src/main/java/com/dispensa/RegisterActivity.kt
+++ app/src/main/java/com/dispensa/RegisterActivity.kt
@@ -2,16 +2,24 @@ package com.dispensa
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.login.*
 import kotlinx.android.synthetic.main.register.*
+import kotlinx.android.synthetic.main.login.inputEmail as inputEmail1
+import kotlinx.android.synthetic.main.login.inputPassword as inputPassword1
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+        auth = FirebaseAuth.getInstance()
 
 
         pickerPeso.setMinValue(40);
@@ -23,10 +31,50 @@ class RegisterActivity : AppCompatActivity() {
         pickerAltezza.wrapSelectorWheel = true
 
         buttonConferma.setOnClickListener {
-            val acc = Intent (this, MainActivity::class.java)
-            startActivity(acc)
+            signUpUser()
+            /*val acc = Intent (this, MainActivity::class.java)
+            startActivity(acc)*/
         }
 
+    }
+
+    private fun signUpUser () {
+
+        if (inputNome.text.toString().isEmpty()) {
+            inputNome.error = "Inserire un nome"
+            inputNome.requestFocus()
+            return
+        }
+
+        if (inputEmail.text.toString().isEmpty()) {
+            inputEmail.error = "Inserire un indirizzo e-mail"
+            inputEmail.requestFocus()
+            return
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(inputEmail.text.toString()).matches()) {
+            inputEmail.error = "Inserire un indirizzo e-mail valido"
+            inputEmail.requestFocus()
+            return
+        }
+
+
+        if (inputPassword.text.toString().isEmpty()) {
+            inputPassword.error = "Inserire una password"
+            inputPassword.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(inputEmail.text.toString(), inputPassword.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(baseContext, "Registrazione fallita. Riprova più tardi",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
 }
