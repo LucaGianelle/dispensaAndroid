dae5ab885cd9d874dd7438e5309099b9ba7ced72
Federico <corna.federico.wkcf@gmail.com>
2021-04-24 11:39:04 +0200

    Mappa inserita

--- app/src/main/java/com/dispensa/MapActivity.kt
+++ app/src/main/java/com/dispensa/MapActivity.kt
@@ -1,6 +1,25 @@
 package com.dispensa
 
-class MapActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
+class  MapActivity : AppCompatActivity() {
 
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
+    //qui si apre il fragment della mappa, può anche essere messo su un altra schermata
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mappa)
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map_frame) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+        })
+    }
 }
