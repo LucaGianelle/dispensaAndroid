4dc7b1d2ca62c70c33fdf79c6d3041c6b0c4c793
Federico <corna.federico.wkcf@gmail.com>
2021-04-24 11:52:46 +0200

    Mappa posizione

--- app/src/main/java/com/dispensa/MapActivity.kt
+++ app/src/main/java/com/dispensa/MapActivity.kt
@@ -1,25 +1,43 @@
 package com.dispensa
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class  MapActivity : AppCompatActivity() {
 
     lateinit var mapFragment: SupportMapFragment
     lateinit var googleMap: GoogleMap
 
-    //qui si apre il fragment della mappa, può anche essere messo su un altra schermata
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mappa)
 
+        //carichiamo il fragment della mappa
+
         mapFragment = supportFragmentManager.findFragmentById(R.id.map_frame) as SupportMapFragment
         mapFragment.getMapAsync(OnMapReadyCallback {
             googleMap = it
+
+
+            /*aggiungere la richiesta dei permessi*/
+            /*googleMap.isMyLocationEnabled = true*/
+
+            //calcoliamo la posizione
+
+            val location1 = LatLng(45.27,9.11)
+            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
+
+            //zoom della camera
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1, 10F))
         })
     }
 }
