88c3216ddb4819e04c086c47664f82514fd5aea5
lgianelle <lgianelle@studenti.uninsubria.it>
2021-05-07 16:10:19 +0200

    memorizzato account loggato

--- app/src/main/java/com/dispensa/MainActivity.kt
+++ app/src/main/java/com/dispensa/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import com.dispensa.RealtimeDatabase.RealTimeActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.load_app.*
 import kotlinx.android.synthetic.main.login.*
 
@@ -15,13 +16,18 @@ class MainActivity : AppCompatActivity() {
 
     /*private lateinit var auth: FirebaseAuth*/
 
+    private lateinit var mFirebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.login)
         setContentView(R.layout.load_app)
        /* auth = FirebaseAuth.getInstance()*/
+        mFirebaseAuth = FirebaseAuth.getInstance()
 
         val buttonStart = findViewById<Button>(R.id.startb)
+
+
 
         //vediamo se funziona --------------------------
         buttonStart.setOnClickListener{
@@ -45,6 +51,20 @@ class MainActivity : AppCompatActivity() {
             startActivity(data)
         }*/
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val mFirebaseUser : FirebaseUser? = mFirebaseAuth.currentUser
+        if(mFirebaseUser != null){
+            val prv1 = Intent (this, HomeActivity::class.java)
+            startActivity(prv1)
+        }else{
+
+            val prv2 = Intent (this, LoginActivity::class.java)
+            startActivity(prv2)
+        }
     }
 
     /*public override fun onStart() {
