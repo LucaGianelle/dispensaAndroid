6e7e04a43e35c653142bb158184e71936f6bf023
lgianelle <lgianelle@studenti.uninsubria.it>
2021-05-08 15:43:24 +0200

    DbCommunication.kt

--- app/src/main/java/com/dispensa/DbCommunication.kt
+++ app/src/main/java/com/dispensa/DbCommunication.kt
@@ -1,16 +1,20 @@
 package com.dispensa
 
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class DbCommunication {
+object DbCommunication {
 
     private lateinit var database: DatabaseReference
-
+    private lateinit var reference: DatabaseReference
 
     fun getDbReference(){
         database = Firebase.database.reference
     }
 
+    fun getDbInstance(){
+
+    }
 }
