2c12f2c6d6a3eee1dd741731dc5ca40859e1534e
Davide <dbasilico1@studenti.uninsubria.it>
2021-05-12 15:59:20 +0200

    RecycleView

---
+++ app/src/main/java/com/dispensa/RealtimeDatabase/ActivityOne.kt
@@ -0,0 +1,12 @@
+package com.dispensa.RealtimeDatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dispensa.R
+
+class ActivityOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one)
+    }
+}
