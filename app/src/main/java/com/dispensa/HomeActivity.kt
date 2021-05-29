76ddfef9d563680da116160706641dd2746e53da
Federico <corna.federico.wkcf@gmail.com>
2021-05-29 16:16:32 +0200

    fix

--- app/src/main/java/com/dispensa/HomeActivity.kt
+++ app/src/main/java/com/dispensa/HomeActivity.kt
@@ -43,6 +43,7 @@ class HomeActivity : AppCompatActivity() {
         val idUtente : String = auth.currentUser.uid
         DbCommunication.setId(idUtente)
 
+        DbCommunication.setCurrentUser()
         DbCommunication.setFoodStorage()
         DbCommunication.setMyaliment()
 
